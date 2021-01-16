1a6f9370836c3340eba2fba70d711479387c3564
Mcelhennyi <mcelhenny_ian@yahoo.com>
2021-01-15 18:34:19 -0600

    Moved mavsdk to symlink in linux file system so the build would work properly

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+[ ! -d "~/thirdparty/mavsdk" ] && \
+    cd ~/thirdparty && \
+    git clone https://github.com/mavlink/MAVSDK.git mavsdk && \
+    cd mavsdk && \
+    git checkout v0.35.1 && \
+    git submodule init && \
+    git submodule update --recursive --init
